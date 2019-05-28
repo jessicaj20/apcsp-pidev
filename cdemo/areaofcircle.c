1c70b7cbd36d76326b913cefd80ef05cf7fe0878
jessicaj20 <50212072+jessicaj20@users.noreply.github.com>
2019-05-27 18:40:32 -0700

    areaofcircle.c

---
+++ cdemo/areaofcircle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float areaofcircle (float r, float a)
+{
+  a = r * r * 3.14;
+  return a;
+}
+
+int main(void) 
+{
+  //call areaofcircle
+  float i;
+  for (i=3.5; i<=12.5; i++)
+  {
+    float r = i;
+    float a;
+    a = areaofcircle(r, a);
+    printf("The area of the circle is %f\n", a);
+  }
+}
